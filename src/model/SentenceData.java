429a2beca36b2479b9c19c9b06b8439f34876aab
kazuman <noblesse-oblige_23@i.softbank.jp>
2015-04-22 13:40:11 +0900

    feat(model): create class, SentenceData

---
+++ src/model/SentenceData.java
@@ -0,0 +1,73 @@
+package model;
+
+public class SentenceData {
+	private String type;
+	private String name;
+	private String class_name;
+	private String comment;
+	private int    line;
+	
+	
+	// ---------------------------------------------
+	// constructor
+	// ---------------------------------------------
+	
+	public SentenceData() {
+		// init
+		this.type 		= "";
+		this.name 		= "";
+		this.class_name = "";
+		this.comment 	= "";
+		this.line 		= -1;
+	}
+	
+	
+	// ---------------------------------------------
+	// getter
+	// ---------------------------------------------
+	
+	public String getType() {
+		return type;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getClassName() {
+		return class_name;
+	}
+	
+	public String getComment() {
+		return comment;
+	}
+	
+	public int getLine() {
+		return line;
+	}
+	
+	
+	// ---------------------------------------------
+	// setter
+	// ---------------------------------------------
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setClassName(String className) {
+		this.class_name = className;
+	}
+	
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	public void setLine(int line) {
+		this.line = line;
+	}
+}
